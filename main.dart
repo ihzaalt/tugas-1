5bb13490487ffa2d390b43a73129e7f24522f7da
ihzaalt <136775124+ihzaalt@users.noreply.github.com>
2023-09-13 22:39:07 +0700

    Update main.dart

--- main.dart
+++ main.dart
@@ -6,10 +6,12 @@ class Mahasiswa {
   Mahasiswa(this.nim, this.nama, this.tahunLahir);
 
   void perkenalan() {
-    print("Perkenalkan, nama saya $nama");
+    int usia = DateTime.now().year - this.tahunLahir;
+    print("Perkenalkan, nama saya $nama dengan nim $nim dan usia $usia tahun");
   }
 }
 
 void main() {
-  var mhs = Mahasiswa("H1D020068", "Abdalhaqq Muhammad Saih", 2002);
+  var mhs = Mahasiswa("H1D020068", "Muhammad Ihza Althaf Ramadhan", 2003);
+  mhs.perkenalan();
 }
